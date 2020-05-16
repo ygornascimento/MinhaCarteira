07f3631b40ee89f911e1c6202babef888dac8974
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-05-16 18:30:37 -0300

    Project bin added

--- MinhaCarteira/MinhaCarteira/ViewController.swift
+++ MinhaCarteira/MinhaCarteira/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let titleLabel: UILabel = {
+            let view = UILabel()
+            view.text = "algo deu errado"
+            view.font = UIFont.boldSystemFont(ofSize: 16.0)
+            view.adjustsFontSizeToFitWidth = true
+            view.textColor = #colorLiteral(red: 0.1647058824, green: 0.1647058824, blue: 0.1647058824, alpha: 1)
+            return view
+        }()
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ])
+
+        view.addSubview(titleLabel)
     }
 
 
